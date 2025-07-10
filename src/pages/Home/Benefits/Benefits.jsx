6f1a0d613ab0a7c44ceecb9ed1c2b94a25d67ed5
Ammar Shahab <ammar.qa@generalpharma.com>
2025-07-10 14:12:01 +0600

    changes colors in  benefits section

--- src/pages/Home/Benefits/Benefits.jsx
+++ src/pages/Home/Benefits/Benefits.jsx
@@ -10,50 +10,44 @@ import {
 const features = [
   {
     title: "Instant Quote Calculation",
-    subtitle: "Fast and Accurate Pricing",
     description:
       "Generate precise quotes instantly with our streamlined calculator, saving you time and effort.",
-    icon: <FaCalculator className="text-3xl mr-3" />,
+    icon: <FaCalculator className="text-4xl text-green-800" />,
     gradient: "from-[#8dd95d] to-green-400",
   },
   {
     title: "Expert Agent Support",
-    subtitle: "Round-the-Clock Assistance",
     description:
       "Connect with our expert agents anytime for personalized guidance and support.",
-    icon: <FaHeadset className="text-3xl mr-3" />,
+    icon: <FaHeadset className="text-4xl text-blue-800" />,
     gradient: "from-[#93c8ed] to-blue-400",
   },
   {
     title: "100% Online Application",
-    subtitle: "Effortless Digital Process",
     description:
       "Apply entirely online with a simple, paperless process designed for convenience.",
-    icon: <FaLaptopCode className="text-3xl mr-3" />,
+    icon: <FaLaptopCode className="text-4xl text-yellow-700" />,
     gradient: "from-[#b9a447] to-yellow-400",
   },
   {
     title: "Secure Online Payments",
-    subtitle: "Safe Transactions Guaranteed",
     description:
       "Pay securely with our encrypted online payment system, ensuring your data is protected.",
-    icon: <FaLock className="text-3xl mr-3" />,
+    icon: <FaLock className="text-4xl text-green-900" />,
     gradient: "from-[#8cda5d] to-green-500",
   },
   {
     title: "Real-Time Claim Tracking",
-    subtitle: "Live Status Updates",
     description:
       "Track your claims in real-time with clear, up-to-date progress reports.",
-    icon: <FaChartLine className="text-3xl mr-3" />,
+    icon: <FaChartLine className="text-4xl text-blue-900" />,
     gradient: "from-[#93c8ed] to-blue-500",
   },
   {
     title: "Personalized Dashboard Access",
-    subtitle: "Your Custom Hub",
     description:
       "Manage your account and preferences through a tailored, user-friendly dashboard.",
-    icon: <FaUserCircle className="text-3xl mr-3" />,
+    icon: <FaUserCircle className="text-4xl text-yellow-800" />,
     gradient: "from-[#b9a447] to-yellow-500",
   },
 ];
@@ -61,8 +55,14 @@ const features = [
 const Benefits = () => {
   return (
     <section className="bg-[#deecf6] dark:bg-gray-900 py-16 px-4 mx-auto">
-      <div className=" mx-auto text-center mb-12">
-        <h2 className="text-3xl sm:text-4xl font-extrabold bg-gradient-to-r from-[#baa53a] to-[#fcd547] bg-clip-text text-transparent">
+      <div className="mx-auto text-center mb-12 mt-10">
+        <h2
+          className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold text-transparent bg-clip-text"
+          style={{
+            backgroundImage:
+              "linear-gradient(#74b5f1, #c6a84e,#70d778,  #c6a84e, #74b5f1)",
+          }}
+        >
           Benefits of Trust Life
         </h2>
         <p className="mt-3 text-gray-600 dark:text-gray-300 max-w-2xl mx-auto text-sm sm:text-base">
@@ -71,18 +71,19 @@ const Benefits = () => {
         </p>
       </div>
 
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="max-w-7xl px-4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {features.map((feature, index) => (
           <div
             key={index}
             className={`bg-gradient-to-br ${feature.gradient} rounded-lg shadow-lg p-6 text-white transition-transform hover:scale-[1.02] duration-300 text-center`}
           >
-            <div className="flex flex-col justify-center items-center mb-4">
+            <div className="flex flex-col justify-center items-center gap-2 mb-4">
               {feature.icon}
-              <h3 className="text-xl font-bold">{feature.title}</h3>
+              <h3 className="text-xl font-bold bg-gradient-to-r from-white to-yellow-100 bg-clip-text text-transparent">
+                {feature.title}
+              </h3>
             </div>
-            <p className="text-sm mb-2 font-semibold">{feature.subtitle}</p>
-            <p className="text-sm">{feature.description}</p>
+            <p className="text-sm text-white">{feature.description}</p>
           </div>
         ))}
       </div>
