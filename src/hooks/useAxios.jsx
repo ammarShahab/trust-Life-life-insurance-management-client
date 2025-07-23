69769543561803ba370b6b4c06353af10be5ba9e
Ashahab007 <ashahab007@gmail.com>
2025-07-23 12:16:33 +0600

    changed the url

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://b11a12-server-side-ammar-shahab.vercel.app",
 });
 
 const useAxios = () => {
